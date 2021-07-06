6cf8edd068399e21483585e08c6edd4e5ec3af7f
website-portfolio <Nannaphat.sudchawa@gmail.com>
2021-07-05 21:39:16 -0700

    Add Pokemon list in Scritp.js file

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,27 @@
-alert('Hello World');
-
-let favoriteFood = 'Pad Thai and Papaya salad';
-document.write(favoriteFood);
+let pokemonList = [
+    {
+        name: "Gengar",
+        height: 1.5,
+        type: ['Ghost' , 'Poison']
+    },
+    {
+        name: "Charizard",
+        height: 1.7,
+        type: ['Fire' , 'Flying']
+    },
+    {
+        name: "Psyduck",
+        height: 0.8,
+        type: 'water'
+    },
+    {
+        name: "Bellossom",
+        height: 0.4,
+        type: 'grass'
+    },
+    {
+        name: "Squirtle",
+        height: 0.4,
+        type: ['Monster', 'Water']
+    }
+];
