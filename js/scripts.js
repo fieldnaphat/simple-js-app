685cad47afd42cccc723850dc442f580d85ce066
website-portfolio <Nannaphat.sudchawa@gmail.com>
2021-07-06 21:48:46 -0700

    - Display pokemon list in array on index page - Add background color to index page and make text center - Highlight special Pokémon who taller than 1 meter display it on index page with highlight text

--- js/scripts.js
+++ js/scripts.js
@@ -25,3 +25,19 @@ let pokemonList = [
         types: ['Monster']
     }
 ];
+
+for (let i of pokemonList) {
+    // console.log(i.name,i.height,i.types);
+
+    if (i.height >= 1) {
+        //Check for which Pokomon is taller than 1 meter dis play it with text ("Wow, that's big!!")
+        document.write(i.name + ' (height ' + i.height + ' M) ' + ' Type ' + i.types + " - Wow, that's big!! " + "<br><br>");
+    } else {
+        //  Display small pokemon here
+        document.write(i.name + ' (height ' + i.height + ' M) ' + ' Type ' + i.types + "<br><br>");
+    }
+}
+
+
+
+
