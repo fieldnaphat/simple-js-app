e7c9131b52ba50467e61875ab4f89f3913f80897
website-portfolio <Nannaphat.sudchawa@gmail.com>
2021-07-13 21:47:28 -0700

    - Create a new pokemonRepository variable for IIFE - Used forEach function for loop in array - Add new pokemon to array ny new function

--- js/scripts.js
+++ js/scripts.js
@@ -1,43 +1,87 @@
-let pokemonList = [
-    {
+// IIFE Part
+let pokemonRepository = (function () {
+
+    let pokemonList = [{
         name: "Cubone",
         height: 0.4,
-        types: ['Monster']
+        types: ["Monster"],
     },
     {
         name: "Charizard",
         height: 1.7,
-        types: ['Fire']
+        types: ["Fire"],
     },
     {
         name: "Psyduck",
         height: 0.8,
-        types: ['Water']
+        types: ["Water"],
     },
     {
         name: "Bellossom",
         height: 0.4,
-        types: ['Grass']
-},
+        types: ["Grass"],
+    },
     {
         name: "Squirtle",
         height: 0.4,
-        types: ['Monster']
+        types: ["Monster"],
+    },
+    ];
+
+    function add(pokemon) {
+        pokemonList.push(pokemon);
     }
-];
 
-for (let pokemon of pokemonList) {
-    // console.log(i.name,i.height,i.types);
+    function getAll() {
+        return pokemonList;
+    }
 
-    if (pokemon.height >= 1) {
-        //Check for which Pokomon is taller than 1 meter dis play it with text ("Wow, that's big!!")
-        document.write(pokemon.name + ' (height ' + pokemon.height + ' M) ' + ' Type ' + pokemon.types + " - Wow, that's big!! " + "<br><br>");
+    return {
+        add: add,
+        getAll: getAll,
+    };
+
+
+    
+})();
+
+pokemonRepository.add({
+    name: "Vulpix",
+    height: 0.6,
+    types: ["Drought"],
+});
+
+pokemonRepository.add({
+    name: "Igglybuff",
+    height: 0.3,
+    types: ["Fairy"],
+});
+
+//Display all the pokemon info from array in console
+// console.log(pokemonRepository.getAll());
+
+//Display all the pokemon info from array
+pokemonRepository.getAll().forEach(function (pokemonList) {
+    const {
+        name,
+        height,
+        types
+    } = pokemonList;
+
+    // Bonus Task
+    //Object key
+    Object.keys(pokemonList).forEach(function (list) {
+        console.log(pokemonList[list]);
+    });
+
+    // console.table(pokemonList);
+
+    if (pokemonList.height >= 1) {
+        document.write(
+            `${name} tall ${height} type ${types} - Wow, that's big!! <br><br>`
+        );
     } else {
-        //  Display small pokemon here
-        document.write(pokemon.name + ' (height ' + pokemon.height + ' M) ' + ' Type ' + pokemon.types + "<br><br>");
+        document.write(`${name} tall ${height} type ${types} <br><br>`);
     }
-}
 
-
-
-
+});
